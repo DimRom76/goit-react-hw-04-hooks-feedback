db652a9d2bb14dd1604f57fb28d3c252143f9956
Dmitriy Kotulsky <66743608+DimRom76@users.noreply.github.com>
2021-02-17 22:02:30 +0200

    error

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ export default function App() {
         />
       </Section>
       <Section title="Statistics">
-        {positivFeedback ? (
+        {totalFeedback ? (
           <Statistics
             good={good}
             neutral={neutral}
